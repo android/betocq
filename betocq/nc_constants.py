af3ae75f9a3556c1ba9657468a3273076614c64e
Google <copybara-servicebot@google.com>
2025-10-01 21:03:41 +0000

    Internal Change.
    PiperOrigin-RevId: 813904215

--- betocq/nc_constants.py
+++ betocq/nc_constants.py
@@ -27,6 +27,8 @@ BETOCQ_NAME = 'BeToCQ'
 
     'com.google.android.nearby.mobly.snippet.dct'
 )
+    'com.google.android.nearby.mobly.snippet.dct.second'
+)
 NEARBY_SNIPPET_PACKAGE_NAME = 'com.google.android.nearby.mobly.snippet'
 NEARBY_SNIPPET_2_PACKAGE_NAME = 'com.google.android.nearby.mobly.snippet.second'
 
